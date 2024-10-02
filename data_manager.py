c146e643ea3e5c5815c97c7454f6a1672f02122d
Andrey Blazejuk <andreyblazejuk@hotmail.com>
2024-10-02 05:17:16 -0300

    Fix DataManager types

--- data_manager.py
+++ data_manager.py
@@ -9,20 +9,20 @@ class DataManager(metaclass=SingletonMeta):
     def __init__(self) -> None:
         self.data = self.load_data()
 
-    def load_data(self):
+    def load_data(self) -> Any:
         if not os.path.exists(self.DATA_FILE):
             return None
         
         with open(self.DATA_FILE, "r") as file:
             return json.load(file)
 
-    def get_value(self, key) -> Any:
+    def get_value(self, key: str) -> Any:
         if self.data and key in self.data:
             return self.data[key]
         
         return None
 
-    def save_data(self, balance, purchases) -> None:
+    def save_data(self, balance: float, purchases: dict[str, float]) -> None:
         data = {
             "balance": balance,
             "purchases": purchases
